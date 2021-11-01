ea2ee642b593b899d390a52cb41f099b69d74458
payal <sasmalpayal@.com>
2021-11-01 20:06:21 +0530

    added classbased view

--- classviewproject/classviewapp/models.py
+++ classviewproject/classviewapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class PassengerModel(models.Model):
+class PassengerClassModel(models.Model):
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=20)
-    trackPoint = models.DecimalField()
+    trackPoint = models.DecimalField(max_digits=3)
 
     def __str__(self) -> str:
         return self.firstName
--- classviewproject/classviewapp/views.py
+++ classviewproject/classviewapp/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
-from classviewproject.classviewapp.models import PassengerModel
+from rest_framework.serializers import Serializer
+from classviewproject.classviewapp.models import PassengerClassModel
 from .serializer import passengerSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
 
 # Create your views here.
-class PassengerDetails:
+class PassengerDetails(APIView):
 
     def get(self, request):
-        passenger = PassengerModel.objects.all()
+        passenger = PassengerClassModel.objects.all()
         serializer = passengerSerializer(passenger, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(requ)
+    def post(self, request):
+        serializer = passengerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PassengerDetailsPKBased(APIView):
+    def get(self, pk):
+        try:
+            return PassengerClassModel.objects.get(id=pk)
+        except return Response
